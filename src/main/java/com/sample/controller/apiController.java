b2a227c93a9c3771a56602f8c4c40dfde769c158
jongjoo kim <jjongju8790@gmail.com>
2020-01-04 19:30:48 +0900

    first

---
+++ src/main/java/com/sample/controller/apiController.java
@@ -0,0 +1,28 @@
+package com.sample.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class apiController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    ResponseEntity<Map<String, String>> sample() {
+        ResponseEntity<Map<String, String>> response = null;
+
+        Map<String, String> resMap = new HashMap<String, String>();
+        resMap.put("type", "First eureka client!");
+        resMap.put("message", "Spring Cloud is awesome!");
+
+        response = new ResponseEntity<Map<String, String>>(resMap, HttpStatus.OK);
+
+        return response;
+    }
+
+}
